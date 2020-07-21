36da1cc8622877a95396f69c4c223596f2ffd346
Diaa Boubaker <ronaldo1927@hotmail.fr>
2020-07-22 00:57:39 +0100

    make it better1

--- scrape/__init__.py
+++ scrape/__init__.py
@@ -5,7 +5,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
-app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://suljevvezarygo:2b8061cd3ee09ef09428764e6db15b206631ba2744d17403855bfd289f1442a7@ec2-54-234-28-165.compute-1.amazonaws.com:5432/d9qtve3lvoe1fg'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://mxktqvdzpeeteg:00b868962d778ae0e85a19e47da434f7413c3f6d84178f2d957b5421e626e613@ec2-35-173-94-156.compute-1.amazonaws.com:5432/d427kpvudu9nnu'
 
 db = SQLAlchemy(app)
 
